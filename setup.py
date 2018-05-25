545b8d8b0a2139e5f6ee7e51479f540424c395a5
Steven Enamakel <enamakel@cryptocontrol.io>
2018-05-25 19:16:45 +0530

    bumpv ersion

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 
 setuptools.setup(
     name = 'crypto_news_api',
-    packages=setuptools.find_packages(),
+    packages=['crypto_news_api'],
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    version = '1.0.3',
+    long_description_content_type='text/markdown',
+    version = '1.0.4',
     description = 'Client to get a crypto newsfeed in your app from the CryptoControl API',
     author = 'Steven Enamakel',
     author_email = 'enamakel@cryptocontrol.io',
     url = 'https://github.com/cryptocontrol/python-api',
-    download_url = 'https://github.com/cryptocontrol/python-api/archive/1.0.3.tar.gz',
-    keywords = ["crypto", "news", "cryptocurrency", "blockchain", "api"],
+    download_url = 'https://github.com/cryptocontrol/python-api/archive/1.0.4.tar.gz',
+    keywords = ['crypto', 'news', 'cryptocurrency', 'blockchain', 'api'],
     classifiers = [],
+    install_reqs = ['requests']
 )
