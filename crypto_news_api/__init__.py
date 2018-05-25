a1611e1d0239943302975db97615b57421675c97
Steven Enamakel <enamakel@cryptocontrol.io>
2018-05-25 18:47:52 +0530

    added a simple py file

---
+++ crypto_news_api/__init__.py
@@ -0,0 +1,21 @@
+class CryptoControlAPI:
+    def __init__(self, apiKey):
+        self.apiKey = apiKey
+
+    def getTopNews(self):
+        pass
+
+    def getLatestNews(self):
+        pass
+
+    def getTopNewsByCategory(self):
+        pass
+
+    def getTopNewsByCoin(self):
+        pass
+
+    def getLatestNewsByCoin(self):
+        pass
+
+    def getTopNewsByCoinCategory(self):
+        pass
---
+++ test.py
@@ -0,0 +1,6 @@
+from crypto_news_api import CryptoControlAPI
+
+c = CryptoControlAPI("helo")
+c.getTopNews()
+c.getTopNews()
+c.getTopNews()
